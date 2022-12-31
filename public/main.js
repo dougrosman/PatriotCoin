f420b88595b456544f7f2015612280c4881af447
dougrosman-edu <drosman@saic.edu>
2022-12-31 13:44:58 -0800

    create public folder

---
+++ public/main.js
@@ -0,0 +1,83 @@
+const CONNECT_AUTOMATICALLY = true;
+let contract;
+let signer;
+let contractWithSigner;
+let connected = false;
+let mintingPaused = false;
+
+// copyrightYear.textContent = `${new Date().getFullYear()}`
+
+if(CONNECT_AUTOMATICALLY) {
+  main();
+} else {
+  connectButton.onclick = main;
+}
+
+
+
+async function main() {
+  loadingIconConnect.style.display = "block";
+
+  // Check website compatibility
+  if(navigator.userAgent.indexOf("Safari") != -1
+  && navigator.userAgent.indexOf("Chrome") == -1) {
+    alert("Please switch to a desktop browser that supports Web3 (Chrome, Firefox, Brave, Edge, or Opera)");
+    loadingIconConnect.style.display = "none";
+    return;
+  }
+  console.log("Browser is Web3 compatible");
+
+  // Check if MetaMask is installed
+  if(!window.ethereum) {
+    alert("No Web3 Provider detected, please install MetaMask (https://metamask.io)");
+    loadingIconConnect.style.display = "none";
+    return;
+  }
+  console.log("MetaMask is installed");
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+  // If the network changes, refresh the page. (e.g. the user switches from mainnet to goerli)
+  provider.on("network", (newNetwork, oldNetwork) => {
+    if (oldNetwork) {
+        window.location.reload();
+    }
+  });
+
+  try {
+    // (REQUIRED) Request to connect current wallet to the dApp
+    await provider.send("eth_requestAccounts", []);
+  } catch(error) {
+    const errorMessage = "Cannot connect to wallet. There might be an issue with another browser extenstion. Try disabling some browser extensions (other than MetaMask), then attempt to reconnect."
+    console.error(errorMessage, error);
+    alert(errorMessage);
+    loadingIconConnect.style.display = "none";
+    return;
+  }  
+  console.log("Wallet connected");
+
+  // Check if user is signed in to correct network
+  const chainId = await provider.getNetwork();
+  if(chainId.chainId != 80001) {
+    alert("Please switch to the Mumbai Polygon Test Network in MetaMask. The page will refresh automatically after switching.");
+    loadingIconConnect.style.display = "none";
+    return;
+  }
+  console.log("Connected to Mumbai");
+  connectionStatus.textContent = "🟢 Connected";
+  connectButton.setAttribute("disabled", "true");
+  signer = provider.getSigner();
+  contract = new ethers.Contract(contractAddress, contractABI, provider);
+
+  // (REQUIRED) Connect the signer to the contract
+  contractWithSigner = contract.connect(signer);
+
+  // Display the address of the signed-in wallet
+  const connectedWalletAddress = await signer.getAddress();
+  console.log(`Connected Wallet: ${connectedWalletAddress}`);
+
+  // hide the loading icon
+  loadingIconConnect.style.display = "none";
+  connected = true;
+}
+
---
+++ public/sketch.js
@@ -0,0 +1,77 @@
+// p5 global variables
+let capture;
+let cnv;
+const w = 320;
+const h = 240;
+
+// ml5js global variables
+let classifier;
+const imageModelURL = '../../models/tm-my-image-model/model.json'
+
+// other global variables
+let meterHeight = 0;
+
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL);
+}
+
+function setup() {
+  cnv = createCanvas(w, h);
+  cnv.parent('sketch');
+  
+  capture = createCapture(VIDEO);
+  capture.size(w, h);
+  capture.parent('sketch');
+
+  pixelDensity(1);
+  noStroke();
+
+  classifyVideo();
+}
+
+function windowResized() {
+  const newWidth = min(640, innerWidth);
+  const newHeight = newWidth * 0.75;
+  resizeCanvas(newWidth, newHeight);
+}
+
+function classifyVideo() {
+  classifier.classify(capture, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error)
+    return;
+  }
+  clear();
+
+  const GROW_RATE = 2;
+  const SHRINK_RATE = 7;
+  const METER_WIDTH = width/8;
+  
+  if(results[0].label=="over_heart" && results[0].confidence > 0.95) {
+    if(meterHeight < height) {
+      meterHeight+=GROW_RATE;
+    }
+  } else {
+    if(meterHeight > 0) {
+      meterHeight-=SHRINK_RATE;
+    }
+  }
+
+  if(meterHeight >= height && !mintingPaused) {
+    contractWithSigner.GIVEUSACOIN();
+    mintingPaused = true;
+  }
+
+  const startColor = color(255, 0, 0);
+  const endColor = color(0, 0, 255);
+  const lerpAmount = map(meterHeight, 0, height, 0, 1);
+  const lerpedColor = lerpColor(startColor, endColor, lerpAmount);
+
+  fill(lerpedColor);
+  rect(width-METER_WIDTH, height-meterHeight, METER_WIDTH, meterHeight);
+
+  classifyVideo();
+}

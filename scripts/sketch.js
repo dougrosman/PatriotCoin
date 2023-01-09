ef0e8905830667b1d77e52eed642b078783e87e6
dougrosman-edu <drosman@saic.edu>
2023-01-09 04:37:56 -0800

    adjust difficulty

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -53,10 +53,10 @@ async function gotResult(error, results) {
   clear();
 
   const GROW_RATE = 5;
-  const SHRINK_RATE = 7;
+  const SHRINK_RATE = 9;
   const METER_WIDTH = width / 8;
 
-  if(results[0].label == "over_heart" && results[0].confidence > 0.65 && !mintingPaused) {
+  if(results[0].label == "over_heart" && results[0].confidence > 0.78 && !mintingPaused) {
   // if (true) {
     if (meterHeight < height) {
       meterHeight += GROW_RATE;
